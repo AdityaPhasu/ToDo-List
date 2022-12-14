ba7765a186073de6b9c1328a3a2dec1be03be577
aditya phasu <adityaphasu919@gmail.com>
2022-12-14 20:49:22 +0530

    first commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        className="add-btn"
+        onClick={() => {
+          if (inputText === "") {
+            return 0;
+          } else {
+            props.onAdd(inputText);
+            setInputText("");
+          }
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { ImBin } from "react-icons/im";
+
+function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <div>
+      <div onClick={handleClick}>
+        <li
+          style={{
+            textDecoration: isDone ? "line-through" : "none"
+          }}
+        >
+          {props.item}
+        </li>
+      </div>
+      <div className="right">
+        <button
+          className="delete-btn"
+          onClick={() => {
+            props.onChecked(props.id);
+          }}
+        >
+          <ImBin className="icon" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ToDoItem;
