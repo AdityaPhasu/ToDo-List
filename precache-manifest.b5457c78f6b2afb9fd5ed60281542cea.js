e4bf35d22ae85ed2def300d5f31a97545a0620dd
aditya phasu <adityaphasu919@gmail.com>
2022-12-14 21:07:47 +0530

    Updates

---
+++ precache-manifest.b5457c78f6b2afb9fd5ed60281542cea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d2c207c399be4487a9ce44426e9292",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d95205382898a81caaa4",
+    "url": "/todo-list/static/js/2.8bff5ffa.chunk.js"
+  },
+  {
+    "revision": "e22b36b33ac1822b5c3d",
+    "url": "/todo-list/static/js/main.f600431d.chunk.js"
+  },
+  {
+    "revision": "d053b0d8f1539185cb3d",
+    "url": "/todo-list/static/js/runtime-main.73ebbad3.js"
+  }
+]);
